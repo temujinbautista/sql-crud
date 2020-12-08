3aa42d5bb15c45febf376aee83d8ca97297f6872
temujinbautista <temujinbautista@gmail.com>
2020-12-08 17:30:01 +0800

    created login form

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'inc/config.php';
+require 'inc/loginStyle.php';
+?>
+
+<div class="login-form">
+    <form action="/examples/actions/confirmation.php" method="post">
+        <h2 class="text-center">Log in</h2>       
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Username" required="required">
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" placeholder="Password" required="required">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-block">Log in</button>
+        </div>      
+    </form>
+</div>
+
+
+<?php
+require 'inc/footer.php';
+?>
