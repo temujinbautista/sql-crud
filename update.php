2fdbc431e065e8d12613c4b23bab8159f2f16d5c
temujinbautista <temujinbautista@gmail.com>
2020-12-09 16:17:50 +0800

    email verification update

--- update.php
+++ update.php
@@ -11,14 +11,14 @@ $row = $results->fetch_assoc();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
     $email = $_POST['email'];
-    $sql = "SELECT * FROM users WHERE email = '$email' LIMIT 1";
-    $results = $conn->query($sql);
-    $row = $results->fetch_assoc();
+    $qry = "SELECT * FROM users WHERE email = '$email' LIMIT 1";
+    $results = $conn->query($qry);
+    $uEmail = $results->fetch_assoc();
     if ($_POST['newPassword'] !== $_POST['confirm']) {
         echo '<script language="javascript">';
         echo 'alert("Password Does Not Match")';
         echo '</script>';
-    } elseif ($email == $row['email']) {
+    } elseif ($email == $uEmail['email'] && $email !== $row['email']) {
         echo '<script language="javascript">';
         echo 'alert("Email Already In Use")';
         echo '</script>';
