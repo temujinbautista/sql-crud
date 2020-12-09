259ec65652143c39ec2c6a92be7c943c556348a6
temujinbautista <temujinbautista@gmail.com>
2020-12-09 14:58:20 +0800

    login confirmation

--- login.php
+++ login.php
@@ -1,20 +1,35 @@
 <?php
 require_once 'inc/config.php';
 require 'inc/loginStyle.php';
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $email = $_POST['email'];
+    $password = md5($_POST['password']);
+    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password' LIMIT 1";
+    $results = $conn->query($sql);
+    $row = $results->fetch_assoc();
+    if ($row['email'] == $email && $row['password'] == $password) {
+        header('Location: index.php?');
+    } else {
+        echo "Error";
+    }
+}
 ?>
 
 <div class="login-form">
-    <form action="/examples/actions/confirmation.php" method="post">
-        <h2 class="text-center">Log in</h2>       
+    <form method="POST">
+        <h2 class="text-center">Log in</h2>
         <div class="form-group">
-            <input type="text" class="form-control" placeholder="Username" required="required">
+            <input type="text" class="form-control" name="email" placeholder="Email" required="required">
         </div>
         <div class="form-group">
-            <input type="password" class="form-control" placeholder="Password" required="required">
+            <input type="password" class="form-control" name="password" placeholder="Password" required="required">
         </div>
         <div class="form-group">
             <button type="submit" class="btn btn-primary btn-block">Log in</button>
-        </div>      
+        </div>
     </form>
 </div>
 
